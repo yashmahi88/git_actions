6968ca0e07d0a88b1f2f6110f23f374f45e4a7e1
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-10-23 18:51:40 +0530

    simple action and core with vercel ncc 8

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,20 +2,28 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  // Log debug, warning, and error messages
   core.debug("Debug Message");
   core.warning("Warning message");
   core.error("Error message");
 
+  // Get the input
   const name = core.getInput("who_to_greet");
-
+  
+  // Log the greeting
   console.log(`Hello ${name}`);
 
+  // Get the current time
   const time = new Date();
+  
+  // Set the output
   core.setOutput("time", time.toTimeString());
 
+  // Export the HELLO_TIME variable
   core.exportVariable("HELLO_TIME", time);
 
-  core.startGroup("Logging github context");
+  // Logging GitHub context
+  core.startGroup("Logging GitHub context");
   console.log(JSON.stringify(github.context, null, 2));
   core.endGroup();
 } catch (error) {
