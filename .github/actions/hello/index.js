2ab8b5fac700856f53ed05f339250605d997e37e
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-10-23 18:44:19 +0530

    simple action and core with vercel ncc 6

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -6,7 +6,7 @@ try{
     const name = core.getInput('who_to_greet');
 
     console.log('Hello ${name}');
-    const time = new Date();
+    //const time = new Date();
     core.setOutput("time",time.toTimeString());
     core.exportVariable("HELLO_TIME", time);
     core.startGroup("Logging github context");
