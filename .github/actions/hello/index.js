71e201cfa6fcbed74c115c09ba8f11a1bc6c1123
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-10-23 18:48:16 +0530

    simple action and core with vercel ncc 7

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,18 +1,23 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-try{
-    core.debug('Debug Message');
-    core.error("Error MEssage");
-    const name = core.getInput('who_to_greet');
 
-    console.log('Hello ${name}');
-    //const time = new Date();
-    core.setOutput("time",time.toTimeString());
-    core.exportVariable("HELLO_TIME", time);
-    core.startGroup("Logging github context");
-    console.log(JSON.stringify(github.context, null ,2));
-    core.endGroup();
+try {
+  core.debug("Debug Message");
+  core.warning("Warning message");
+  core.error("Error message");
 
-} catch(error) {
-    core.setFailed(error.message);
+  const name = core.getInput("who_to_greet");
+
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.exportVariable("HELLO_TIME", time);
+
+  core.startGroup("Logging github context");
+  console.log(JSON.stringify(github.context, null, 2));
+  core.endGroup();
+} catch (error) {
+  core.setFailed(error.message);
 }
