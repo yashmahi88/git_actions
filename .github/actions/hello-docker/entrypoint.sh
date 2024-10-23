eb29140ccf1279628998f43abec0b28051b64988
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-10-23 22:22:56 +0530

    hello docker 4

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "::debug::Running entrypoint.sh"
 echo "Hello $1"
 echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
-echo "HELLO: $HELLO
+echo "HELLO: $HELLO"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$time" >> $GITHUB_ENV
